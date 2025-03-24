c43fb3b3ad7516c6144c3c022460ca8724d26bff
ChiWai Yau <chiwaiyauu@gmail.com>
2025-03-24 07:06:17 -0700

    chore: add missed postman collection

--- backend/app/decorators/event_log_decorator.py
+++ backend/app/decorators/event_log_decorator.py
@@ -7,11 +7,13 @@ from app.repositories.user_repository import UserRepository
 def log_event_decorator(event_type: EventType):
     def decorator(fn):
         @wraps(fn)
-        def wrapper(user_id: str, *args, **kwargs):
-            response = fn(user_id, *args, **kwargs)
-            # Only log if route executed successfully
+        def wrapper(*args, **kwargs):
+            response = fn(*args, **kwargs)
+
             if isinstance(response, tuple) and response[1] < 400:
-                print(event_type.value)
+                from flask import g  
+                user_uuid = getattr(g, 'user_id', None)
+
                 log_event_task.delay(
                     event=event_type.value,
                     request_meta={
@@ -21,7 +23,7 @@ def log_event_decorator(event_type: EventType):
                         "request_data": request.get_json(silent=True),
                         "request_param": request.args.to_dict(),
                     },
-                    user_id = None if user_id is None else UserRepository().get_id_by_uuid(user_id)
+                    user_id=None if user_uuid is None else UserRepository().get_id_by_uuid(user_uuid)
                 )
             return response
         return wrapper
